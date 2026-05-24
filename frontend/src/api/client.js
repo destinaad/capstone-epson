d86e22162d8eb8b94d27af20cff9e3650fa96f12
destinaad <destinadameria@gmail.com>
2026-05-24 20:50:05 +0700

    fix: add https protocol to baseURL in client js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'capstone-epson-production.up.railway.app';
+const baseURL = process.env.REACT_APP_API_URL || 'https://capstone-epson-production.up.railway.app';
 
 export const api = axios.create({
   baseURL,
