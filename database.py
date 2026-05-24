7be2518c6d16b047b21bed382bc02e6194e7f953
destinaad <destinadameria@gmail.com>
2026-05-24 19:46:10 +0700

    fix: match supabase connection strictly

--- database.py
+++ database.py
@@ -3,10 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "postgresql://postgres:#Mj9mL6W5.f8b.D@localhost:5432/epson_qc"
-)
+# Kita buat sistem pengecekan yang sangat ketat
+if os.getenv("RAILWAY_ENVIRONMENT"):
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:#Mj9mL6W5.f8b.D@db.zzcedmweuokpoeoynfrl.supabase.co:5432/postgres"
+else:
+    # SAAT DI LAPTOP LOKAL: Tetap pakai Docker localhost kamu
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pastibisa@localhost:5432/epson_qc"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
