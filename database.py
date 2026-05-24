0325a67dd0a041b40cc34b2b7cd84b4d793e9591
destinaad <destinadameria@gmail.com>
2026-05-24 20:00:02 +0700

    fix: switch supabase port to 6543 for connection pooling

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Kita buat sistem pengecekan yang sangat ketat
 if os.getenv("RAILWAY_ENVIRONMENT"):
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:#Mj9mL6W5.f8b.D@db.zzcedmweuokpoeoynfrl.supabase.co:5432/postgres"
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:#Mj9mL6W5.f8b.D@db.zzcedmweuokpoeoynfrl.supabase.co:6543/postgres"
 else:
     # SAAT DI LAPTOP LOKAL: Tetap pakai Docker localhost kamu
     SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pastibisa@localhost:5432/epson_qc"
