541ccb76f2204074f7849003f1f226081c78df19
destinaad <destinadameria@gmail.com>
2026-05-24 20:08:48 +0700

    fix: use correct supabase pooler host and port combination

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Kita buat sistem pengecekan yang sangat ketat
+# Kita buat sistem pengecekan yang sangat ketat 
 if os.getenv("RAILWAY_ENVIRONMENT"):
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:#Mj9mL6W5.f8b.D@db.zzcedmweuokpoeoynfrl.supabase.co:6543/postgres"
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres.zzcedmweuokpoeoynfrl:#Mj9mL6W5.f8b.D@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
 else:
     # SAAT DI LAPTOP LOKAL: Tetap pakai Docker localhost kamu
     SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pastibisa@localhost:5432/epson_qc"
